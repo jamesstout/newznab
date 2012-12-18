9ade0edfde78c8e007aba175c34b474e11ffc444
jonnyboy <jonnyboy@jbmatrix.net>
2012-12-18 03:55:30 -0500

    Update my_newznab.sh
    added sql commands to change retention when backfilling and nzb import

--- my_newznab.sh
+++ my_newznab.sh
@@ -11,9 +11,12 @@ export NZBS="/media/newznab/batch"  #path to your nzb files
 export MyUSER="root" #mysql user
 export MyPASS="dfgrfgurge" #mysql password
 export DATABASE="newznab"
-export MAXDAYS=730  #max days for backfill
+export MAXDAYS=180  #max days for backfill
+export MAXRET=2  #max days for backfill
 export MYSQL="$(which mysql)"
-export MYSQL_CMD="UPDATE groups set backfill_target=backfill_target+1 where active=1 and backfill_target<$MAXDAYS;"
+export MYSQL_CMD1="UPDATE groups set backfill_target=backfill_target+1 where active=1 and backfill_target<$MAXDAYS;"
+export MYSQL_CMD2="UPDATE site set value=$MAXRET where setting='rawretentiondays';"
+export MYSQL_CMD3="UPDATE site set value=0 where setting=;rawretentiondays';"
 
 LASTOPTIMIZE=`date +%s`
 
@@ -22,17 +25,21 @@ while :
  do
 CURRTIME=`date +%s`
 cd ${NEWZNAB_PATH}
+${MYSQL} -u ${MyUSER} -p${MyPASS} ${DATABASE} -e "${MYSQL_CMD2}"
 /usr/bin/php5 ${NEWZNAB_PATH}/update_binaries_threaded.php
 /usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
+${MYSQL} -u ${MyUSER} -p${MyPASS} ${DATABASE} -e "${MYSQL_CMD3}"
 /usr/bin/php5 ${NEWZNAB_ADMIN_PATH}/nzb-importmodified.php ${NZBS} true
 /usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
-/usr/bin/php5 ${NEWZNAB_PATH}/backfill_threaded.php
-/usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
+#/usr/bin/php5 ${NEWZNAB_PATH}/backfill_threaded.php
+#/usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
+${MYSQL} -u ${MyUSER} -p${MyPASS} ${DATABASE} -e "${MYSQL_CMD2}"
 /usr/bin/php5 ${NEWZNAB_PATH}/update_predb.php true
 /usr/bin/php5 ${NEWZNAB_PATH}/update_parsing.php
 /usr/bin/php5 ${NEWZNAB_PATH}/removespecial.php
 /usr/bin/php5 ${NEWZNAB_PATH}/update_cleanup.php
-${MYSQL} -u ${MyUSER} -p${MyPASS} ${DATABASE} -e "${MYSQL_CMD}"
+${MYSQL} -u ${MyUSER} -p${MyPASS} ${DATABASE} -e "${MYSQL_CMD1}"
+
 
 DIFF=$(($CURRTIME-$LASTOPTIMIZE))
 if [ "$DIFF" -gt 43200 ] || [ "$DIFF" -lt 1 ]

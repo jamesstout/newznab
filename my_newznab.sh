28cac175410fc3a073d121b174895152546a14c0
jonnyboy <jonnyboy@jbmatrix.net>
2012-12-18 03:06:35 -0500

    Update my_newznab.sh
    added update_predb.php to the loop

--- my_newznab.sh
+++ my_newznab.sh
@@ -28,6 +28,7 @@ cd ${NEWZNAB_PATH}
 /usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
 /usr/bin/php5 ${NEWZNAB_PATH}/backfill_threaded.php
 /usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
+/usr/bin/php5 ${NEWZNAB_PATH}/update_predb.php true
 /usr/bin/php5 ${NEWZNAB_PATH}/update_parsing.php
 /usr/bin/php5 ${NEWZNAB_PATH}/removespecial.php
 /usr/bin/php5 ${NEWZNAB_PATH}/update_cleanup.php

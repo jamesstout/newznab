61a282770eff5bfc52f9c1490da977e1266122ea
jonnyboy <jonnyboy@jbmatrix.net>
2012-12-26 21:19:08 -0800

    Create postprocess.sh

---
+++ postprocess.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# run this script as su or sudo
+# it will modify lib/postprocess.php to run only once
+# it will modify lib/releases.php to not run lib/postprocess.php
+# it will download justpostprocessing.php and place it in the correct location
+# this will remove the post processing to it's own loop
+# and will signaificantly decrease time per loop using my scrips
+
+
+# Set these variables - case significant
+NEWZPATH=/var/www/newznab
+WGET=`which wget`
+SED=`which sed`
+
+$WGET -N -O $NEWZPATH/misc/update_scripts/justpostprocessing.php https://dl.dropbox.com/u/8760087/justpostprocessing.php
+$SED -i -e 's/100/1/g' $NEWZPATH/www/lib/postprocess.php
+$SED -i -e 's/$postprocess = new PostProcess(true);/\/\/$postprocess = new PostProcess(true);/' $NEWZPATH/www/lib/releases.php
+$SED -i -e 's/$postprocess->processAll();/\/\/$postprocess->processAll();/' $NEWZPATH/www/lib/releases.php
